b140487b19d3bd37ef046f69f4da5f3db6be1420
eniverz <mspt5871@protonmail.com>
2026-05-22 12:50:19 +0800

    init for config in mac

---
+++ config/fish/conf.d/alias.fish
@@ -0,0 +1,30 @@
+# System command
+alias ls="eza --group-directories-first --icons=always --color=always"
+alias ll="eza --group-directories-first --icons=always --color=always -alH --git"
+alias cls="/usr/bin/clear"
+alias t="eza --group-directories-first --icons=always --color=always --tree"
+alias cat="bat"
+alias rm="rm -i"
+# core command
+alias fetch="fastfetch"
+# utils
+alias untar="tar -zxvf"
+alias untarxz="tar -xvf"
+alias vim="nvim"
+# directory
+abbr ... --position anywhere '../..'
+abbr .... --position anywhere '../../..'
+abbr ..... --position anywhere '../../../..'
+abbr ...... --position anywhere '../../../../..'
+# git
+alias lg="lazygit"
+alias gc="git clone --recursive"
+alias gcm="git commit -m"
+alias gl="git log --oneline"
+alias gp="git push"
+alias gpl="git pull"
+alias gaa="git add ."
+alias gfx="git commit --fixup"
+alias gi="git init"
+alias gb="git branch"
+alias gs="git status"
